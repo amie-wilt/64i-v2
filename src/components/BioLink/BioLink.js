8a6c1eb999f7e81d65740a09ab4e5239ca32dcf8
Dan Wilt <dwilt4rville@gmail.com>
2016-05-08 00:39:25 -0700

    cleaned up bio link

--- src/components/BioLink/BioLink.js
+++ src/components/BioLink/BioLink.js
@@ -1,18 +1,21 @@
 import React, {Component, PropTypes} from 'react';
 import styles from './BioLink.scss';
 import Avatar from '../Avatar/Avatar';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 
-export default class Bio extends Component {
+class BioLink extends Component {
 
     render() {
-        var employee = this.props.employee;
-        
+        var { employee } = this.props;
+        var href = `/about-us/employees/${employee.id}`;
+
         return (
-            <Link className={styles.bio} itemScope="" itemType="http://schema.org/Person" to={`/about-us/employees/${employee.id}`}>
-                <Avatar className={styles['bio-photo']} src={`/public/${employee.id}.jpg`} alt={employee.name} />
+            <Link className={styles.bio} itemScope="" itemType="http://schema.org/Person" to={href}>
+                <Avatar className={styles['bio-photo']} src={`/public/${employee.id}.jpg`} alt={employee.name}/>
                 <span className={styles['bio-name']} itemProp="name">{employee.name}</span>
             </Link>
         )
     }
 }
+
+export default BioLink;
