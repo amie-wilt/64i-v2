ee0cb8aa528eb65baca38341ec1965faf74b850c
Joe Yuhas <joseph.yuhas@gmail.com>
2016-06-15 01:23:42 -0700

    contact form styling besides select

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import reasons from '../../../data/contact-reason-options';
+import styles from './ContactForm.css';
 
 var ContactForm = ({ onSubmit, onChange, name, comments, reason, isSubmitting, showSuccessMessage }) => {
     var successMessage = showSuccessMessage ? (
@@ -11,13 +12,13 @@ var ContactForm = ({ onSubmit, onChange, name, comments, reason, isSubmitting, s
     var reasonsOptions = reasons.map(reason => (<option key={reason.name} value={reason.value}>{reason.name}</option>));
 
     var form = !isSubmitting ? (
-        <form action="/contact-form" onSubmit={onSubmit}>
-            <input type="text" value={name} onChange={onChange} name="name"/>
-            <select value={reason} onChange={onChange} name="reason">
+        <form className={styles.form} action="/contact-form" onSubmit={onSubmit}>
+            <input className={styles.input} type="text" value={name} onChange={onChange} name="name" placeholder="Name"/>
+            <select className={styles.select} value={reason} onChange={onChange} name="reason">
                 {reasonsOptions}
             </select>
-            <textarea value={comments} onChange={onChange} name="comments"></textarea>
-            <button type="submit">Submit</button>
+            <textarea className={styles.textarea} value={comments} onChange={onChange} name="comments" placeholder="Comments"></textarea>
+            <button className={styles.submit} type="submit">Submit</button>
         </form>
     ) : null;
 
