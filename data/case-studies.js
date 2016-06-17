ec0e31ebb4cbdf63852ef51330c3f64a980b95cb
Dan Wilt <dwilt4rville@gmail.com>
2016-06-17 14:25:07 -0700

    put in tourrs case study

--- data/case-studies.js
+++ data/case-studies.js
@@ -3,5 +3,10 @@ export default [
         id: 'amazon-app-store',
         image: '/public/case-studies/amazon-app-store/link.png',
         name: 'Amazon App Store'
+    },
+    {
+        id: 'tourrs-mvp',
+        image: '/public/case-studies/tourrs/link.png',
+        name: 'TOURRS MVP'
     }
 ]
